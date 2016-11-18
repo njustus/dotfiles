437287d0e5b3e551b5bdf8592d29a80873c85615
N. Justus <nicola.justus@mni.thm.de>
2016-11-18 14:19:21 +0100

    create symlinks instead of hardlinks

--- setup.sh
+++ setup.sh
@@ -1,27 +1,29 @@
 # setup the environment by copying & HARDLINKING the files
 
+current=$(pwd)
+
 echo "setup bash"
-ln -f ./bashrc ~/.bashrc
-ln -f ./bash_aliases ~/.bash_aliases
-cp -f ./env_variables ~/.env_variables
-cp -f ./system_config ~/.system_config
+ln -sf $current/bashrc ~/.bashrc
+ln -sf $current/bash_aliases ~/.bash_aliases
+cp -f $current/env_variables ~/.env_variables
+cp -f $current/system_config ~/.system_config
 
 echo "setup git"
-ln -f ./gitconfig ~/.gitconfig
-ln -f ./gitignore_global ~/.gitignore_global
+ln -sf $current/gitconfig ~/.gitconfig
+ln -sf $current/gitignore_global ~/.gitignore_global
 
 echo "setup vim"
-ln -f ./vimrc ~/.vimrc
-cp -r ./vim ~/vim
+ln -sf $current/vimrc ~/.vimrc
+cp -r $current/vim ~/vim
 rm -r ~/.vim
 mv ~/vim ~/.vim
 
 echo "setup emacs"
-ln -f ./emacs ~/.emacs
+ln -sf $current/emacs ~/.emacs
 
 echo "setup sbt plugins"
 sbt_plugins_path=~/.sbt/0.13/plugins
 mkdir -p $sbt_plugins_path
-ln -f ./sbt/plugins.sbt $sbt_plugins_path/plugins.sbt
+ln -sf $current/sbt/plugins.sbt $sbt_plugins_path/plugins.sbt
 
 echo "=== set your system in ~/.env_variables using $SYSTEM"
