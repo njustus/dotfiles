b78606750d9882934fc417c187c0da77cef4145a
N. Justus <nicola.justus@mni.thm.de>
2017-12-01 13:55:51 +0100

    Update themes

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,3 +1,3 @@
 (if (eq system-type 'gnu/linux)
-  (setq prelude-theme 'doom-one)
-  (setq prelude-theme 'base16-atelier-sulphurpool-light))
+    (setq prelude-theme 'base16-unikitty-dark)
+  (setq prelude-theme 'base16-unikitty-light))
