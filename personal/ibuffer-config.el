8604b1202ae3ced52ba140a9ac059690103eb5e6
N. Justus <nicola.justus@mni.thm.de>
2018-04-07 18:35:57 +0200

    update ibuffer groups

---
+++ personal/ibuffer-config.el
@@ -0,0 +1,15 @@
+(require 'ibuffer)
+(setq ibuffer-saved-filter-groups
+      '(("default"
+         ("controllers" (filename . "app/controllers/"))
+         ("models" (filename . "app/models/"))
+         ("views" (filename . "app/views/"))
+         ("emacs" (mode . emacs-lisp-mode))
+         )))
+
+(add-hook 'ibuffer-mode-hook
+	  '(lambda ()
+             (ibuffer-auto-mode 1)
+             (ibuffer-switch-to-saved-filter-groups "default")
+             (message "ibuffer hook applied"))
+)
