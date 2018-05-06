f4ba8a61f304042537f4110501d9a947b0f1eb71
N. Justus <nicola.justus@mni.thm.de>
2018-05-06 09:09:05 +0200

    update theme

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,4 +1,4 @@
 (if (eq system-type 'gnu/linux)
-    (setq prelude-theme 'base16-atelier-forest)
+    (setq prelude-theme 'base16-oceanicnext)
   ;(setq prelude-theme 'base16-atelier-forest-light))
-  (setq prelude-theme 'base16-atelier-forest))
+  (setq prelude-theme 'base16-oceanicnext))
