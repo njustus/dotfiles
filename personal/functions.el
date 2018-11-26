74895330930b8758fb18ce3613f537516e5516c0
N. Justus <nicola.justus@mni.thm.de>
2018-11-26 10:14:57 +0100

    update theme, add opacity function

---
+++ personal/functions.el
@@ -0,0 +1,3 @@
+
+(defun personal-translucent-window ()
+  (set-frame-parameter (selected-frame) 'alpha '(70 50)))
--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,4 +1,4 @@
 (if (eq system-type 'gnu/linux)
-    (setq prelude-theme 'doom-nord-light)
+   (setq prelude-theme 'doom-city-lights)
   ;(setq prelude-theme 'base16-atelier-forest-light))
   (setq prelude-theme 'doom-nord-light))
