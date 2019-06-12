2d68593bdf1f5c90d8ba599acd54b511b5f04f37
N. Justus <nicola.justus@mni.thm.de>
2019-06-12 13:31:21 +0200

    comment lisp & lsp mode

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -23,8 +23,8 @@
 ;; (require 'prelude-haskell)
 (require 'prelude-js)
 ;; (require 'prelude-latex)
-(require 'prelude-lisp)
-(require 'prelude-lsp)
+;;(require 'prelude-lisp)
+;;(require 'prelude-lsp)
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 ;(require 'prelude-perl)
