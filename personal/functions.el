45fa57243f0010020e45da27581c5096eb662c7f
N. Justus <nicola.justus@mni.thm.de>
2019-07-07 12:01:09 +0200

    add function for opaque windows

--- personal/functions.el
+++ personal/functions.el
@@ -2,3 +2,7 @@
 (defun personal-translucent-window ()
   (interactive)
   (set-frame-parameter (selected-frame) 'alpha '(70 50)))
+
+(defun personal-opaque-window ()
+  (interactive)
+  (set-frame-parameter (selected-frame) 'alpha '(100 100)))
