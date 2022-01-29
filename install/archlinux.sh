d42664bc5c9f28622a97d2dc86868cd5d5e5ab00
N. Justus <n-justus@outlook.com>
2022-01-29 16:23:32 +0100

    update archlinux packages

--- install/archlinux.sh
+++ install/archlinux.sh
@@ -2,23 +2,32 @@
 #installs all additional packages on archlinux based systems
 
 packages=(
+	zsh
+  tmux
   vim
+  htop
+  exa
+  emacs
+  code
   evince
   firefox
   thunderbird
   chromium
   vlc
-  clementine
   tree
   ruby
   python
-  jdk8-openjdk
   java-openjfx
   scala
   sbt
   gradle
   texlive-most
   biber
+  keepassxc
+  neofetch
+  telegram-desktop
+  virtualbox
+  libreoffice-fresh
   )
 
 ides=(
@@ -27,6 +36,12 @@ ides=(
   )
 
 read -p "install ides?[y/n] " withIdes
+
+echo "installing prelude.."
+curl -L https://git.io/epre | sh
+
+echo "installing zsh.."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 pacman -Sy #update db
 for package in ${packages[@]};
